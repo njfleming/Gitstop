7b544f7d221698277e83fcd29a99445d750fa0d2
gealarcon <geovannialarcon92@gmail.com>
2021-05-04 19:43:56 -0700

    added comments page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NativeRouter, Route, Link, Switch } from "react-router-native";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Home from './Client/Containers/Home';
 import Login from './Client/Containers/Login';
 import Mention from './Client/Containers/Mention';
@@ -10,16 +11,18 @@ import Repos from './Client/Containers/Repos';
 
 const App = () => {
   return (
-    <NativeRouter>
-        <View style={styles.container}>
-          
-            <Route exact path="/" component={Login}></Route>
-            <Route path="/home" component={Home}></Route>
-            <Route path="/mentions" component={Mention}></Route>
-            <Route path="/user" component={User}></Route>
-            <Route path="/repos/:id" component={Repos}></Route>
-        </View>
-    </NativeRouter>
+    <SafeAreaProvider>
+      <NativeRouter>
+          <View style={styles.container}>
+            
+              <Route exact path="/" component={Login}></Route>
+              <Route path="/home" component={Home}></Route>
+              <Route path="/mentions" component={Mention}></Route>
+              <Route path="/user" component={User}></Route>
+              <Route path="/repos/:id" component={Repos}></Route>
+          </View>
+      </NativeRouter>
+    </SafeAreaProvider>
   );
 }
 
